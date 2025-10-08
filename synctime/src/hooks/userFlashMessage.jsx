88ae6c2be787988709010eec0c2ac35a529b4c1c
Bergkley <bergkley@gmail.com>
2025-10-07 22:13:54 -0300

    feat: adicionar componente de mensagem flash e hook para gerenciamento de mensagens

--- synctime/src/hooks/userFlashMessage.jsx
+++ synctime/src/hooks/userFlashMessage.jsx
@@ -3,6 +3,7 @@ import bus from "../utils/bus";
 export default function useFlashMessage() {
 
     function setFlashMessage(msg, type) {
+        console.log('msg',msg)
         bus.emit('flash', {
             message: msg, 
             type: type})
