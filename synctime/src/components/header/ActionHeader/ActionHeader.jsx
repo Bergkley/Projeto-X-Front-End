a75953fa8fb916927133dd3ea03e3a789447d1fa
Bergkley <bergkley@gmail.com>
2025-10-15 17:46:22 -0300

    feat: add o componente ActionHeader

---
+++ synctime/src/components/header/ActionHeader/ActionHeader.jsx
@@ -0,0 +1,34 @@
+import { ChevronLeft } from "lucide-react";
+import styles from './ActionHeader.module.css';
+import { useEmphasisColor } from "../../../hooks/useEmphasisColor";
+
+const ActionHeader = ({ onBack, onCreate, backButtonLabel = "Voltar", createButtonLabel = "Criar" }) => {
+  const { emphasisColor } = useEmphasisColor();
+
+  return (
+    <div className={styles.actionBar}>
+      <button 
+        className={styles.backButton} 
+        onClick={onBack}
+        aria-label={backButtonLabel}
+        style={{
+          color: emphasisColor || 'rgb(20, 18, 129)',
+
+        }}
+      >
+        <ChevronLeft size={20} />
+      </button>
+      <button 
+        className={styles.createButton} 
+        onClick={onCreate}
+        style={{
+          color: emphasisColor || 'rgb(20, 18, 129)',
+        }}
+      >
+        {createButtonLabel}
+      </button>
+    </div>
+  );
+};
+
+export default ActionHeader;
