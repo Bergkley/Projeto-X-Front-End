1be350b4cf48505d2b266bee54139a64d53fe5ad
Bergkley <bergkley@gmail.com>
2025-10-14 21:22:06 -0300

    feat: add theme footer

--- synctime/src/components/footer/Footer.jsx
+++ synctime/src/components/footer/Footer.jsx
@@ -1,8 +1,11 @@
 import styles from './Footer.module.css';
+import { useTheme } from '../../hooks/useTheme'; 
 
 const Footer = () => {
+  const { theme } = useTheme();
+
   return (
-    <footer className={styles.footer}>
+    <footer className={`${styles.footer} ${styles[theme]}`}>
       <div className={styles.container}>
         <p className={styles.copyright}>
           © {new Date().getFullYear()} <strong>SynTime</strong>. Todos os direitos reservados.
