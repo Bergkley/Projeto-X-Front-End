80c78163e98b4bc6beceb4bfc2df4de6157c088e
Bergkley <bergkley@gmail.com>
2025-10-14 21:22:17 -0300

    feat: add theme container

--- synctime/src/components/layout/Container.jsx
+++ synctime/src/components/layout/Container.jsx
@@ -1,8 +1,10 @@
 import styles from './Container.module.css';
-
+import { useTheme } from '../../hooks/useTheme'; 
 const Container = ({ children }) => {
+  const { theme } = useTheme();
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${styles[theme]}`}>
       <div className={styles.content}>
         {children}
       </div>
