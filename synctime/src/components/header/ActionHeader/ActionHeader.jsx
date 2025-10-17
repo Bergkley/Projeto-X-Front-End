5fe452e5d8601d36e2589ba8822b78d3fa932003
Bergkley <bergkley@gmail.com>
2025-10-17 15:44:11 -0300

    feat: implementando  a propriedade isOnlyBack

--- synctime/src/components/header/ActionHeader/ActionHeader.jsx
+++ synctime/src/components/header/ActionHeader/ActionHeader.jsx
@@ -1,38 +1,45 @@
 // ⚙️ Bibliotecas externas
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft } from 'lucide-react';
 
 // 💅 Estilos
 import styles from './ActionHeader.module.css';
 
 // 🧠 Hooks customizados
-import { useEmphasisColor } from "../../../hooks/useEmphasisColor";
+import { useEmphasisColor } from '../../../hooks/useEmphasisColor';
 
-
-const ActionHeader = ({ onBack, onCreate, backButtonLabel = "Voltar", createButtonLabel = "Criar" }) => {
+const ActionHeader = ({
+  onBack,
+  onCreate,
+  backButtonLabel = 'Voltar',
+  createButtonLabel = 'Criar',
+  isOnlyBack = false
+}) => {
   const { emphasisColor } = useEmphasisColor();
+  console.log('isOnlyBack', isOnlyBack);
 
   return (
     <div className={styles.actionBar}>
-      <button 
-        className={styles.backButton} 
+      <button
+        className={styles.backButton}
         onClick={onBack}
         aria-label={backButtonLabel}
         style={{
-          color: emphasisColor || 'rgb(20, 18, 129)',
-
+          color: emphasisColor || 'rgb(20, 18, 129)'
         }}
       >
         <ChevronLeft size={20} />
       </button>
-      <button 
-        className={styles.createButton} 
-        onClick={onCreate}
-        style={{
-          color: emphasisColor || 'rgb(20, 18, 129)',
-        }}
-      >
-        {createButtonLabel}
-      </button>
+      {!isOnlyBack ? (
+        <button
+          className={styles.createButton}
+          onClick={onCreate}
+          style={{
+            color: emphasisColor || 'rgb(20, 18, 129)'
+          }}
+        >
+          {createButtonLabel}
+        </button>
+      ) : null}
     </div>
   );
 };
