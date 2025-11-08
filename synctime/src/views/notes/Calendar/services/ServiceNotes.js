ef6fe66ba844040fa0a47c2a57b3a53ec74d1f38
Bergkley <bergkley@gmail.com>
2025-11-08 12:41:21 -0300

    feat: conectando com back e reformulando o formulario

---
+++ synctime/src/views/notes/Calendar/services/ServiceNotes.js
@@ -0,0 +1,29 @@
+import api from "../../../../services/api";
+
+class ServiceNotes {
+  getByAllNotes() {
+    const params = new URLSearchParams();
+    
+
+    return api.get(`/notes/userId?${params.toString()}`, {
+      params: { isListAll: true },
+    });
+  }
+  getByIdNotes(id) {
+    return api.get(`/notes/${id}`);
+  }
+
+  createNotes(data) {
+    return api.post(`/notes/create`, data);
+  }
+
+  editNotes(id, data) {
+    return api.patch(`/notes/edit/${id}`, data);
+  }
+
+  deleteNotes(id) {
+    return api.delete(`/notes/delete/${id}`);
+  }
+}
+
+export default new ServiceNotes();
